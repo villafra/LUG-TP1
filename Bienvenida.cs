9e099f19a4199a5ee9a65a71610ab81ace3fe19e
Franco Villafañe <FrancoMiguel.Villafane@alumnos.uai.edu.ar>
2022-05-02 11:03:06 -0300

    Agregar archivos de proyecto.

---
+++ Bienvenida.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Aspecto;
+
+namespace LUG_TP1
+{
+    public partial class frmBienvenida : Form
+    {
+        public frmBienvenida()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Menu.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Aspecto;
+using Negocio;
+
+namespace LUG_TP1
+{
+    public partial class frmMenu : Form
+    {
+        Restó restó = new Restó();
+        public frmMenu()
+        {
+            InitializeComponent();
+            Formatear.FormatearForm(this, panelMenuIzq, this.Width, this.Height);
+            frmBienvenida frm = new frmBienvenida();
+            Formatear.AbrirNuevoForm(this, frm);
+            
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void btnLayout_Click(object sender, EventArgs e)
+        {
+            Form frm = Application.OpenForms.Cast<Form>().FirstOrDefault(x => x is frmLayout);
+            if (frm != null)
+            {
+                frm.BringToFront();
+                return;
+            }
+            else
+            {
+                frm = new frmLayout();
+                Formatear.AbrirNuevoForm(this, frm);
+            }
+           
+            
+        }
+
+        private void MesasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form frm = Application.OpenForms.Cast<Form>().FirstOrDefault(x => x is frmMesas);
+            if (frm != null)
+            {
+                frm.BringToFront();
+                return;
+            }
+            else
+            {
+                frm = new frmMesas();
+                Formatear.AbrirNuevoForm(this, frm);
+            }
+        }
+    }
+}
